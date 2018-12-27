9174213b8e5ccc7ea54d10e2eb982fc48ae37a13
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2018-12-27 15:53:13 -0800

    Add join slack redirect page

--- scripts/copy-docs.sh
+++ scripts/copy-docs.sh
@@ -8,5 +8,4 @@ do
    yarn copyfiles -u 5 "../go/src/github.com/argoproj/$proj/**/*.{md,gif,jpg,png}" .tmp/docs && rm -rf ".tmp/docs/$proj/vendor"
 done
 
-yarn copyfiles "../argo-ci/**/*.{md,gif,jpg,png}" .tmp/docs/argo-ci && node ./scripts/index-docs.js .tmp/docs > static/searchIndex.json
-rm -rf .tmp/docs/argo-ci/node_modules
+yarn copyfiles "../argo-ci/**/*.{md,gif,jpg,png}" .tmp/docs/argo-ci && rm -rf .tmp/docs/argo-ci/node_modules && node ./scripts/index-docs.js .tmp/docs > static/searchIndex.json
