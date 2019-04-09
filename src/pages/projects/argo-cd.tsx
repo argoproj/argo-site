bbd3b017cf11402e8259191204657479a39e77a7
Alex Collins <alex_collins@intuit.com>
2019-04-09 08:53:54 -0700

    fix links

--- src/pages/projects/argo-cd.tsx
+++ src/pages/projects/argo-cd.tsx
@@ -13,16 +13,10 @@ export default ({ data }: {data: any}) => {
 <h2 id="why-argo-cd-">Why Argo CD?</h2>
 <p>Application definitions, configurations, and environments should be declarative and version controlled.
 Application deployment and lifecycle management should be automated, auditable, and easy to understand.</p>
-<h2 id="who-uses-argo-cd-">Who uses Argo CD?</h2>
-<p>Organizations below are <strong>officially</strong> using Argo CD. Please send a PR with your organization name if you are using Argo CD.</p>
-<ol>
-<li><a href="https://www.intuit.com/">Intuit</a></li>
-<li><a href="https://www.yieldlab.de/">Yieldlab</a></li>
-</ol>
 <h2 id="documentation">Documentation</h2>
-<p>To learn more about Argo CD <a href="https://argoproj.github.io/argo-cd/">go to the complete documentation</a>.</p>
+<p>To learn more about Argo CD <a href="hhttps://github.com/argoproj/argo-cd/">go to the complete documentation</a>.</p>
 '
-              docsLink='https://argoproj.github.io/argo-cd/'/>
+              docsLink='https://github.com/argoproj/argo-cd/'/>
         </Main>
     );
 };
