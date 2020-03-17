ce7775311d191840d0865c43d12c1436e1497d32
Alexander Matyushentsev <Alexander_Matyushentsev@intuit.com>
2020-03-17 00:17:57 -0700

    use https://clearbit.com/ logos

---
+++ scripts/prepare.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+./scripts/copy-docs.sh
+./scripts/logos.sh > src/assets/clearbit-logos.txt
+
--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -4,63 +4,66 @@ import * as React from 'react';
 import { PROJECTS } from '../projects';
 
 const argoWheelImg = require('../../assets/images/argo-wheel.png');
-const communities = {
-  'intuit.png': 'https://www.intuit.com/',
-  'blackrock.png': 'https://www.blackrock.com/',
-  'google.png': 'https://www.google.com/intl/en/about/our-company/',
-  'nvidia.png': 'https://www.nvidia.com/',
-  'datadog.png': 'https://www.datadoghq.com/',
-  'cyrus.png': 'https://cyrusbio.com/',
-  'gladly.png': 'https://gladly.com/',
-  'corefilling.png': 'https://www.corefiling.com/',
-  'adoby.png': 'https://www.adobe.com/',
-  'interline.png': 'https://www.interline.io/blog/scaling-openstreetmap-data-workflows/',
-  'pfn.png': 'https://preferred-networks.jp/en/',
-  'tesla.png': 'https://tesla.com/',
-};
-const logos = Object.keys(communities).map((name, index) => ({ name, src: require(`../../assets/images/logos/${name}`), href: communities[name]}));
 
 require('./home.scss');
 
-export const Home = () => (
-    <div className='home'>
-        <div className='home__intro'>
-            <div id='stars'/>
-            <div id='stars2'/>
-            <div id='stars3'/>
-            <div className='main__container'>
-                <div className='row'>
-                    <div className='columns small-8'>
-                        <h1>Get stuff done<br/> with Kubernetes</h1>
-                        <h2>Open source Kubernetes native workflows, events, CI and CD</h2>
-                    </div>
-                    <div className='columns small-4'>
-                        <img src={argoWheelImg} alt='Argo'/>
-                    </div>
-                </div>
-                <div className='home__boxes'>
-                    {Object.keys(PROJECTS).map((proj) => PROJECTS[proj]).map((item) => (
-                        <Link key={item.description} to={item.link} className='home__box'>
-                            <h3>{item.title}</h3>
-                            <p>{item.description}</p>
-                        </Link>
-                    ))}
-                </div>
-            </div>
-        </div>
-        <div className='main__container'>
-            <div className='home__community'>
-                <div className='title'>Join the growing community</div>
-                <div className='home__community-logos'>
-                    {logos.map((item) => (
-                        <div key={item.name} className='home__community-logos__item'>
-                            <a href={item.href} target='_blank'>
-                              <img src={item.src} alt={item.name}/>
-                            </a>
+const data: string = require('raw-loader!../../assets/clearbit-logos.txt');
+const manuallyAddedLogos = require('../../assets/logos.json');
+
+export const Home = () => {
+    const logos = Array.from(new Set(data.split('\n').filter((url) => !!url))).map((domain) => ({
+        url: `https://${domain}`,
+        src: `https://logo.clearbit.com/${domain}`,
+    })).concat(Object.keys(manuallyAddedLogos).filter((domain) => !!manuallyAddedLogos[domain]).map((domain) => ({
+        url: `https://${domain}`,
+        src: require(`../../assets/images/logos/${manuallyAddedLogos[domain]}`),
+    })));
+
+    logos.sort((first, second) => first.url.replace(/^(https:\/\/www\.)/, '').localeCompare(second.url.replace(/^(https:\/\/www\.)/, '')));
+
+    return (
+        <div className='home'>
+            <div className='home__intro'>
+                <div id='stars'/>
+                <div id='stars2'/>
+                <div id='stars3'/>
+                <div className='main__container'>
+                    <div className='row'>
+                        <div className='columns small-8'>
+                            <h1>Get stuff done<br/> with Kubernetes</h1>
+                            <h2>Open source Kubernetes native workflows, events, CI and CD</h2>
                         </div>
-                    ))}
+                        <div className='columns small-4'>
+                            <img src={argoWheelImg} alt='Argo'/>
+                        </div>
+                    </div>
+                    <div className='home__boxes'>
+                        {Object.keys(PROJECTS).map((proj) => PROJECTS[proj]).map((item) => (
+                            <Link key={item.description} to={item.link} className='home__box'>
+                                <h3>{item.title}</h3>
+                                <p>{item.description}</p>
+                            </Link>
+                        ))}
+                    </div>
                 </div>
             </div>
+            <div className='main__container'>
+                <div className='home__community'>
+                    <div className='title'>Join the growing community</div>
+                    <div className='home__community-logos'>
+                        {logos.map((item) => (
+                            <div key={item.url} className='home__community-logos__item'>
+                                <a href={item.url} target='_blank'>
+                                  <img src={item.src} alt={item.url}/>
+                                </a>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <p>
+                    <a href='https://clearbit.com'>Logos provided by Clearbit</a>
+                </p>
+            </div>
         </div>
-    </div>
-);
+    );
+};
