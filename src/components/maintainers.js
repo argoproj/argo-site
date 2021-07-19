cf114828c8d283b5f2489f1c8d512c8ab56521a9
Yuan Tang <terrytangyuan@gmail.com>
2021-07-19 11:16:26 -0400

    docs: Add missing maintainers that have logos in the repo
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- src/components/maintainers.js
+++ src/components/maintainers.js
@@ -9,7 +9,7 @@ const Maintainers = () => {
         allMarkdownRemark(
           filter: {
             fileAbsolutePath: {
-              regex: "/(members/codefresh|intuit|blackrock|redhat)/"
+              regex: "/(members/codefresh|intuit|blackrock|redhat|alibaba|antgroup|preferred)/"
             }
           }
           sort: { order: ASC, fields: frontmatter___title }
