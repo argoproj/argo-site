def15fde1c68283f97c8975eb696e68f5ff25e29
Hong Wang <wanghong230@gmail.com>
2021-10-06 14:58:05 -0700

    feat(maintainers): add maintainer
    Signed-off-by: Hong Wang <wanghong230@gmail.com>

--- src/components/maintainers.js
+++ src/components/maintainers.js
@@ -9,7 +9,7 @@ const Maintainers = () => {
         allMarkdownRemark(
           filter: {
             fileAbsolutePath: {
-              regex: "/(members/codefresh|intuit|blackrock|redhat)/"
+              regex: "/(members/codefresh|intuit|blackrock|redhat|akuity)/"
             }
           }
           sort: { order: ASC, fields: frontmatter___title }
