b53bc1dc6107b837da4d17d5917a5805b5db32fc
todaywasawesome <dan@codefresh.io>
2022-04-19 09:48:13 -0600

    Add ArgoCon '22 banner
    Signed-off-by: todaywasawesome <dan@codefresh.io>

--- src/components/header.js
+++ src/components/header.js
@@ -25,6 +25,18 @@ const Header = ({ color }) => {
 
     <React.Fragment>
 
+      <div className="announcement-banner bg-[#302871] py-3">
+        <a href="https://events.linuxfoundation.org/argocon/">
+          ArgoCon '22 CFP and Registration now open! Join us in the SF Bay Area or virtually 9/19 - 9/21
+          <svg class="HoverArrow" width="10" height="10" viewBox="0 0 10 10" aria-hidden="true">
+            <g fill-rule="evenodd">
+            <path class="HoverArrow__linePath" d="M0 5h7"></path>
+            <path class="HoverArrow__tipPath" d="M1 1l4 4-4 4"></path>
+            </g>
+          </svg>
+        </a>
+      </div>
+
       <header className={headerClass}>
         <div className="container flex items-center">
           <Link to="/">
