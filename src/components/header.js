7a69fb497b4cc3efbfaf0e2537bf4dffd86ee4b0
wojtekidd <wojtek.cichon@protonmail.com>
2023-08-22 15:50:24 +0200

    Changed wording
    Signed-off-by: wojtekidd <wojtek.cichon@protonmail.com>

--- src/components/header.js
+++ src/components/header.js
@@ -27,7 +27,7 @@ const Header = ({ color }) => {
 
       <div className="announcement-banner bg-[#302871] py-3">
         <a href="https://events.linuxfoundation.org/kubecon-cloudnativecon-north-america/co-located-events/argocon/">
-          ArgoCon NA 2023 is now a co-located event at KubeCon/CloudNativeCon 2023 Chicago. CFP is now open!
+          ArgoCon NA 2023 is now a co-located event at KubeCon/CloudNativeCon 2023 Chicago. Find out more!
           <svg class="HoverArrow" width="10" height="10" viewBox="0 0 10 10" aria-hidden="true">
             <g fill-rule="evenodd">
             <path class="HoverArrow__linePath" d="M0 5h7"></path>
