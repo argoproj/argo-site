655a7a9cd50e385da121285e889b0be38986f3cd
Michael Crenshaw <350466+crenshaw-dev@users.noreply.github.com>
2026-08-24 11:06:05 -0400

    remove unnecessary sanitization

--- redirects/redirect-html.js
+++ redirects/redirect-html.js
@@ -1,26 +1,15 @@
-function escapeHtml(value) {
-  return value
-    .replace(/&/g, "&amp;")
-    .replace(/"/g, "&quot;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-}
-
 function redirectHtml({ title, url }) {
-  const safeTitle = escapeHtml(title)
-  const safeUrl = escapeHtml(url)
-
   return `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="refresh" content="0; url=${safeUrl}">
-  <link rel="canonical" href="${safeUrl}">
-  <title>${safeTitle} - Argo Documentation</title>
+  <meta http-equiv="refresh" content="0; url=${url}">
+  <link rel="canonical" href="${url}">
+  <title>${title} - Argo Documentation</title>
   <script>window.location.replace(${JSON.stringify(url)})</script>
 </head>
 <body>
-  <p>Redirecting to <a href="${safeUrl}">${safeTitle} documentation</a>…</p>
+  <p>Redirecting to <a href="${url}">${title} documentation</a>…</p>
 </body>
 </html>
 `
